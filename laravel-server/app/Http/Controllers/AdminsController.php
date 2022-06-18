263ffad93b7e99df0ed8010791c8352df2f1e7c5
AliOlliek <olliek95@gmail.com>
2022-06-18 11:00:43 +0300

    Admin Can Upload Survey

--- laravel-server/app/Http/Controllers/AdminsController.php
+++ laravel-server/app/Http/Controllers/AdminsController.php
@@ -5,28 +5,46 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Survey;
 use App\Models\Question;
+use App\Models\Answer;
 
 class AdminsController extends Controller
-{
+{   
     function createSurveys(Request $request){
-        $check = Survey::where("name", "=", $request->title)->get();
+        $check = Survey::where("title", "=", $request["surveyTitle"])->get();
         if (!$check){
             $status = "Exists";
             return $status;
         }
+
         $survey = new Survey;
-        $survey -> title = $request -> surveyTitle;
-        $survey -> created_by = $request -> surveyCreatedBy;
+        $survey -> title = $request["surveyTitle"];
+        $survey -> created_by = $request["surveyCreatedBy"];
+        $survey->save();
 
-        foreach($question as $request->$questions){
-            $question = new Question;
-            $question -> content = $request->$questions-> content; 
-            $question -> question_type = $request->$question->question_type;
-
-            foreach($answer as $request->$questions->$answers){
+        for($i = 0; $i < count($request["questions"]) ; $i++){
+                $question = new Question;
+                $question -> content = $request["questions"][$i][0];
+                $question -> question_type = $request["questions"][$i][1];
+                $question -> survey_question_id = $survey->id;
+                $question->save();
+            
+            for($j = 0; $j < count($request["questions"][$i][2]); $j++){     
                 $answer = new Answer;
-                $answer -> content = $request->$questions->$answers-> content;
-            }
-        }
+                $answer -> content = $request["questions"][$i][2][$j];
+                $answer -> question_answer_id = $question->id;
+                $answer -> save();
+            };
+        };
+
+        return response()->JSON([
+                "status" => "success",
+                "Added" => "Survey, Questions and Answers"
+            ], 200);
     }
 }
+
+
+
+
+
+
