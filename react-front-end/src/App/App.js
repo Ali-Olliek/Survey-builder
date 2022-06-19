e754ca5d96a05732e79d0ea6478049b00cc951be
AliOlliek <olliek95@gmail.com>
2022-06-19 09:09:34 +0300

    Created Basic Login Page

---
+++ react-front-end/src/App/App.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from "react";
+import '../stylesheets/App.css';
+import Login from '../components/Login'
+
+function App() {
+
+  return (
+      <Login/>
+  );
+}
+
+export default App;
---
+++ react-front-end/src/components/Login.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+
+export default function Login() {
+      const [values, setValues] = useState({
+        Username: "",
+        Email: "",
+        Password: "",
+      });
+
+      const [submitted, setSubmitted] = useState(false);
+      const [valid, setValid] = useState(false);
+
+      const handleUserNameInput = (event) => {
+        setValues({ ...values, Username: event.target.value });
+      };
+
+      const handleEmailInput = (event) => {
+        setValues({ ...values, Email: event.target.value });
+      };
+
+      const handlePasswordInput = (event) => {
+        setValues({ ...values, Password: event.target.value });
+      };
+
+      const handleSubmit = (event) => {
+        event.preventDefault();
+        if (values.Username && values.Email && values.Password) {
+          setValid(true);
+        }
+        setSubmitted(true);
+      };
+      
+  return (
+    <div className="form-container">
+      <form className="register-form" onSubmit={handleSubmit}>
+        {submitted && valid ? (
+          <div className="success-message">
+            Success! Thank you for registering
+          </div>
+        ) : null}
+        <input
+          onChange={handleUserNameInput}
+          value={values.Username}
+          className="form-field"
+          placeholder="Username"
+          name="username"
+        />
+        {submitted && !values.Username ? (
+          <span>Please enter a username</span>
+        ) : null}
+        <input
+          onChange={handleEmailInput}
+          value={values.Email}
+          className="form-field"
+          placeholder="Email"
+          name="Email"
+        />
+        {submitted && !values.Email ? <span>Please enter an Email</span> : null}
+        <input
+          onChange={handlePasswordInput}
+          value={values.password}
+          className="form-field"
+          type={"password"}
+          placeholder="Password"
+          name="Password"
+        />
+        {submitted && !values.Password ? (
+          <span>Please enter a password</span>
+        ) : null}
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
