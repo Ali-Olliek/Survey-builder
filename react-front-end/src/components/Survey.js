6858981231657f7e3ba05f13203cc780ac32eadf
AliOlliek <olliek95@gmail.com>
2022-06-19 20:09:33 +0300

    Created Survey Page to Get A Single Survey

--- react-front-end/src/components/Survey.js
+++ react-front-end/src/components/Survey.js
@@ -1,1 +1,24 @@
+import React, { useState, useEffect} from 'react'
+import axios from 'axios'
 
+export default function Survey() {
+    const [singleSurvey, setSingleSurvey] = useState(["NO DATA"]);
+    const url = "http://127.0.0.1:8000/api/v1/Surveys/DisplaySurvey2";
+    useEffect(() => {     
+     axios({
+         method:"GET",
+         url: url
+        }).then(res =>setSingleSurvey(res.data))
+    }, []);  
+
+console.log(singleSurvey)
+return (
+   <div className="Survey-container">
+     {singleSurvey.map((survey) => {
+       return (
+        survey
+       );
+     })}
+   </div>
+ );
+  }
