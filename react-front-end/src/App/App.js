d8f8315f4fd57570dac451d56ddc0a36e31c02a0
AliOlliek <olliek95@gmail.com>
2022-06-19 18:07:36 +0300

    Added and Imported Creating Survey Component

--- react-front-end/src/App/App.js
+++ react-front-end/src/App/App.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import '../stylesheets/App.css';
 import Login from '../components/Login';
 import Surveys from '../components/Surveys';
 import SignUp from '../components/SignUp';
+import CreateSurvey from '../components/CreateSurvey';
+import DropDown from '../components/DropDown';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
 
+  const [selected, setSelected] = useState("");
+  
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />}>
         </Route>
+        <Route path="/SignUp" element={<SignUp />}>
+        </Route>
         <Route path="/Surveys" element={<Surveys />}>
         </Route>
-        <Route path="/SignUp" element={<SignUp />}>
+        <Route path="/CreateSurvey" element={<CreateSurvey selected={selected} setSelected = {setSelected}/>}>
         </Route>
       </Routes>
     </Router>
