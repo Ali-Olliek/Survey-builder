1b41a706e1cef8fa9ca2a4199b10b8ef9fe2057b
AliOlliek <olliek95@gmail.com>
2022-06-20 22:55:10 +0300

    Added function To Display a Survey with Styling

--- react-front-end/src/components/Survey.js
+++ react-front-end/src/components/Survey.js
@@ -3,28 +3,51 @@ import axios from 'axios'
 
 export default function Survey() {
 
-    const [singleSurvey, setSingleSurvey] = useState(["NO DATA"]);
-    const id = localStorage.getItem("Survey_Id");
-    const [currentPageUrl, setCurrentPageUrl] = useState(`http://127.0.0.1:8000/api/v1/Surveys/DisplaySurvey${id}`);
+  const [surveyInfo, setSurveyInfo] = useState({
+    title: "",
+    createdBy: "",
+    questions: [],
+    answers: [],
+  })
 
-    useEffect(() => {
-      axios.get(currentPageUrl)
-        .then((res) => {
-          console.log(res.data)
-          setSingleSurvey(res.data.map((survey) => survey));
+  const id = localStorage.getItem("Survey_Id");
+  const [currentPageUrl, setCurrentPageUrl] = useState(`http://127.0.0.1:8000/api/v1/Surveys/DisplaySurvey${id}`);
+
+  useEffect(() => {
+    axios.get(currentPageUrl)
+      .then((res) => {
+        console.log(res.data)
+        setSurveyInfo({
+          title: res.data.survey.title,
+          createdBy: res.data.survey.created_by,
+          questions: res.data.survey_questions,
+          answers: res.data.survey_answers
         });
-      }, [currentPageUrl]);
-      
-let data = Array.from(singleSurvey)
+      });
+    }, [currentPageUrl]);
+
+console.log(surveyInfo.answers)
+
+let i = 0;
+
 return (
-  <div>
-    {data.map(survey => {
-      return (
-        <>
-          <h1>{JSON.stringify(survey)}</h1>
-        </>
-        )
-      })}
+  <div className='sur'>
+    <div className="surveyEle">
+      <div>
+        <h2 className="surveyInfo">{surveyInfo.title}</h2>
+        <p className="surveyInfo">{surveyInfo.createdBy}</p>
+      </div>
+      <div className="questions">
+        {surveyInfo.questions.map((question) => (
+          <p className="question">
+            {question.content}
+            {surveyInfo.answers[i].map((answer) => (
+              <h4 className="answer">{answer.content}</h4>
+            ))}
+          </p>
+        ))}
+      </div>
     </div>
-  );
+  </div>
+);
 }
