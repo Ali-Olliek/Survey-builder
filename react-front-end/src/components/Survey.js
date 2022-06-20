2f3ac3adbfa96eabad88179fadf8f8498735cab4
AliOlliek <olliek95@gmail.com>
2022-06-20 08:24:05 +0300

    Created Survey Component with Linking To DB

--- react-front-end/src/components/Survey.js
+++ react-front-end/src/components/Survey.js
@@ -2,23 +2,29 @@ import React, { useState, useEffect} from 'react'
 import axios from 'axios'
 
 export default function Survey() {
-    const [singleSurvey, setSingleSurvey] = useState(["NO DATA"]);
-    const url = "http://127.0.0.1:8000/api/v1/Surveys/DisplaySurvey2";
-    useEffect(() => {     
-     axios({
-         method:"GET",
-         url: url
-        }).then(res =>setSingleSurvey(res.data))
-    }, []);  
 
-console.log(singleSurvey)
+    const [singleSurvey, setSingleSurvey] = useState(["NO DATA"]);
+    const id = localStorage.getItem("Survey_Id");
+    const [currentPageUrl, setCurrentPageUrl] = useState(`http://127.0.0.1:8000/api/v1/Surveys/DisplaySurvey${id}`);
+
+    useEffect(() => {
+      axios.get(currentPageUrl)
+        .then((res) => {
+          console.log(res.data)
+          setSingleSurvey(res.data.map((survey) => survey));
+        });
+      }, [currentPageUrl]);
+      
+let data = Array.from(singleSurvey)
 return (
-   <div className="Survey-container">
-     {singleSurvey.map((survey) => {
-       return (
-        survey
-       );
-     })}
-   </div>
- );
-  }
+  <div>
+    {data.map(survey => {
+      return (
+        <>
+          <h1>{JSON.stringify(survey)}</h1>
+        </>
+        )
+      })}
+    </div>
+  );
+}
