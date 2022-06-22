58862572b1af642d5ec46e459c05251b8a5a09f1
AliOlliek <olliek95@gmail.com>
2022-06-22 20:47:00 +0300

    Created NavBar

---
+++ react-front-end/src/components/NavBar.js
@@ -0,0 +1,31 @@
+import react from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+
+const Navbar = () => {
+    const location = useLocation();
+    const navTabs = [
+        { link: "/admin/surveys", name: "Admin" },
+        { link: "/user/surveys ", name : "User" },
+    ]
+
+return (
+    <nav className='Top-Bar'>
+        <ul className='Tab'>
+            {navTabs.map((navTab, index) => {
+                return (
+                    <NavLink key = {index} to = {navTab.link}>
+                        <li>{navTab.name}</li>
+                    </NavLink>
+                );
+            })}
+            {location.pathname.includes("admin") && (
+                <NavLink to = "/admin/CreateSurveys">
+                    <li>Create Survey</li>
+                </NavLink>
+            )}
+        </ul>
+    </nav>
+)
+}
+
+export default Navbar;
